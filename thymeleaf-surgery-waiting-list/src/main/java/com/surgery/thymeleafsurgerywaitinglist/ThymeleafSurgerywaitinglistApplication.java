f81fd23baca0b92d8341ece1dd1cad625c5a2103
AdelLinux80s <80212462+AdelLinux80s@users.noreply.github.com>
2022-04-07 20:37:14 +0300

    project starter

---
+++ thymeleaf-surgery-waiting-list/src/main/java/com/surgery/thymeleafsurgerywaitinglist/ThymeleafSurgerywaitinglistApplication.java
@@ -0,0 +1,13 @@
+package com.surgery.thymeleafsurgerywaitinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSurgerywaitinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSurgerywaitinglistApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-surgery-waiting-list/src/test/java/com/surgery/thymeleafsurgerywaitinglist/ThymeleafSurgerywaitinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surgery.thymeleafsurgerywaitinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSurgerywaitinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
