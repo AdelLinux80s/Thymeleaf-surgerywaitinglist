bb0f3d5da7e0149dbcfa2bc74f7ac64aa682c407
AdelLinux80s <80212462+AdelLinux80s@users.noreply.github.com>
2022-04-07 21:14:13 +0300

    Patient Class

---
+++ thymeleaf-surgery-waiting-list/src/main/java/com/surgery/thymeleafsurgerywaitinglist/entity/Patient.java
@@ -0,0 +1,18 @@
+package com.surgery.thymeleafsurgerywaitinglist.entity;
+
+public class Patient {
+
+	private Long patientId;
+	private Long patientNationalID;
+	private String patientFirstName;
+	private String patientLastName;
+	private String patientNationality;
+	private Date patientDateOfBirth;
+	private String patientGender;
+	private String maritalStatus;
+	private String patientAddress;
+	private String patientConact;
+	private String patientEmailAddress;
+	private String patientBloodType;
+	private String patientOccupation;
+}
